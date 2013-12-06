43466e957a7ac4f2d0f90d0d373f71969ce878dc
Ilton Garica <ton.garcia.jr@gmail.com>
2013-12-06 18:00:13 -0200

    retirada do cache (forcando direto no browser via meta)

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
+    <META HTTP-EQUIV="Pragma" CONTENT="no-cache">
 
     <!-- Le styles -->
     <!-- Folhas de estilo -->
